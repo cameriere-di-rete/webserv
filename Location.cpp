ae73c224a6c89ef1e3f3ef1b043704cb93d03b97
S-0-L-0 <nonsocheemailscrivere9@gmail.com>
2025-11-11 16:26:28 +0100

    cambiate variabili per location e server, aggiunto config

--- Location.cpp
+++ Location.cpp
@@ -1,16 +1,45 @@
 #include "Location.hpp"
 
-Location::Location() : path(), directives() {}
+Location::Location()
+    : allow_methods(), redirect_code(0),
+      redirect_location(), cgi(false), index(), autoindex(false), root(),
+      error_page() {
+  // default allowed methods
+  allow_methods.insert(GET);
+  allow_methods.insert(POST);
+  allow_methods.insert(PUT);
+  allow_methods.insert(DELETE);
+  allow_methods.insert(HEAD);
+}
 
-Location::Location(const std::string &p) : path(p), directives() {}
+Location::Location(const std::string &p)
+    : allow_methods(), redirect_code(0),
+      redirect_location(), cgi(false), index(), autoindex(false), root(),
+      error_page() {
+  // default allowed methods
+  allow_methods.insert(GET);
+  allow_methods.insert(POST);
+  allow_methods.insert(PUT);
+  allow_methods.insert(DELETE);
+  allow_methods.insert(HEAD);
+}
 
 Location::Location(const Location &other)
-    : path(other.path), directives(other.directives) {}
+    : allow_methods(other.allow_methods), redirect_code(other.redirect_code),
+      redirect_location(other.redirect_location), cgi(other.cgi),
+      index(other.index), autoindex(other.autoindex), root(other.root),
+      error_page(other.error_page) {}
 
 Location &Location::operator=(const Location &other) {
   if (this != &other) {
-    path = other.path;
-    directives = other.directives;
+    allow_methods = other.allow_methods;
+    redirect_code = other.redirect_code;
+    redirect_location = other.redirect_location;
+    cgi = other.cgi;
+    index = other.index;
+    autoindex = other.autoindex;
+    root = other.root;
+    error_page = other.error_page;
   }
   return *this;
 }
--- Location.hpp
+++ Location.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -11,7 +12,17 @@ public:
   Location(const Location &other);
   Location &operator=(const Location &other);
   ~Location();
+  // Types
+  enum Method { GET, POST, PUT, DELETE, HEAD };
 
-  std::string path;
-  std::map<std::string, std::vector<std::string> > directives;
+  // Location-specific configuration
+  std::set<Method> allow_methods;            // allowed methods (default: GET, POST, PUT, DELETE, HEAD)
+  int redirect_code;                  // numeric redirect status code (e.g. 301, 302)
+  std::string redirect_location;             // location to redirect to
+  bool cgi;                                  // whether CGI is enabled for this location
+  std::set<std::string> index;               // index files (e.g. index.html)
+  bool autoindex;                            // enable directory listing
+  std::string root;                          // filesystem root for this location
+  std::map<int, std::string> error_page; // custom error page mapping
+
 };
