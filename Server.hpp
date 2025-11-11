382ce059e2813869555ad0983a73c6c2f376dba1
S-0-L-0 <nonsocheemailscrivere9@gmail.com>
2025-11-11 12:45:29 +0100

    fix operator >>

--- Server.hpp
+++ Server.hpp
@@ -17,7 +17,7 @@ public:
   int fd;           // Server socket file descriptor
   int port;         // Port number to listen on
   std::string host; // Host address to bind to
-  std::map<std::string, std::vector<std::string>>
+  std::map<std::string, std::vector<std::string> >
       directives; // Server directives (root, index, autoindex, etc.)
   std::map<std::string, Location> locations; // Location blocks mapped by path
 
