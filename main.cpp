b928295e15ee86e06ee7d0f95c0d5891b3764df8
S-0-L-0 <nonsocheemailscrivere9@gmail.com>
2025-11-19 22:08:59 +0000

    refactor: move argument processing functions to utils.cpp and update logic for clarity

--- main.cpp
+++ main.cpp
@@ -7,58 +7,7 @@
 #include "Config.hpp"
 #include "Logger.hpp"
 #include "ServerManager.hpp"
-
-int parseLogLevelFlag(const std::string& arg) {
-  // Guard 1: Wrong length
-  if (arg.length() != 4) {
-    return -1;
-  }
-
-  // Guard 2: Wrong prefix
-  if (arg.compare(0, 3, "-l:") != 0) {
-    return -1;
-  }
-
-  // Guard 3: Invalid value
-  char level = arg[3];
-  if (level < '0' || level > '2') {
-    return -1;
-  }
-
-  // All good
-  return level - '0';
-}
-
-// Parse program arguments and fill `path` and `logLevel`.
-// This was moved out of main to keep main shorter and clearer.
-void processArgs(int argc, char** argv, std::string& path, int& logLevel) {
-  // Defaults
-  logLevel = 1;  // Default: INFO
-
-  bool logFlagSet = false;
-
-  // Parse arguments
-  for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
-    int level = parseLogLevelFlag(arg);
-
-    if (level >= 0) {
-      if (!logFlagSet) {
-        logLevel = level;
-        logFlagSet = true;
-      }
-    } else {
-      if (path.empty()) {
-        path = arg;
-      } else {
-        throw std::runtime_error("Error: multiple config file paths provided");
-      }
-    }
-  }
-  if (path.empty()) {
-    path = "./webserv.conf";
-  }
-}
+#include "utils.hpp"
 
 int main(int argc, char** argv) {
   // run `./webserv -l:N` to choose the log level
--- utils.hpp
+++ utils.hpp
@@ -15,3 +15,13 @@ std::string trim_copy(const std::string& s);
 // Initialize a set with the default allowed HTTP methods
 // (GET, POST, PUT, DELETE, HEAD)
 void initDefaultHttpMethods(std::set<http::Method>& methods);
+
+// Default configuration file path
+const char* DEFAULT_CONFIG_PATH;
+
+// Parse log level flag (e.g., "-l:0" for DEBUG, "-l:1" for INFO, "-l:2" for ERROR)
+int parseLogLevelFlag(const std::string& arg);
+
+// Parse program arguments and fill `path` and `logLevel`.
+// This was moved out of main to keep main shorter and clearer.
+void processArgs(int argc, char** argv, std::string& path, int& logLevel);
