775bc101e40c7f642017324d7ec639c766e043fd
S-0-L-0 <nonsocheemailscrivere9@gmail.com>
2025-11-25 15:02:59 +0100

    style: improve code formatting and reorder includes in RedirectHandler

--- RedirectHandler.cpp
+++ RedirectHandler.cpp
@@ -1,16 +1,13 @@
 #include "RedirectHandler.hpp"
 
+#include "Connection.hpp"
 #include "HttpStatus.hpp"
+#include "RedirectHandler.hpp"
 #include "Response.hpp"
 #include "constants.hpp"
 
-#include "RedirectHandler.hpp"
-#include "Connection.hpp"
-
-#include "Response.hpp"
-#include "HttpStatus.hpp"
-
-RedirectHandler::RedirectHandler(const Location& location) : location_(location) {}
+RedirectHandler::RedirectHandler(const Location& location)
+    : location_(location) {}
 
 RedirectHandler::~RedirectHandler() {}
 
@@ -18,7 +15,8 @@ HandlerResult RedirectHandler::start(Connection& conn) {
   // Prepare redirect response and serialize into connection write buffer
   conn.response = Response();
   conn.response.status_line.status_code = location_.redirect_code;
-  conn.response.status_line.reason = http::reasonPhrase(location_.redirect_code);
+  conn.response.status_line.reason =
+      http::reasonPhrase(location_.redirect_code);
   conn.response.addHeader("Location", location_.redirect_location);
   conn.response.getBody().data = "";
   conn.response.addHeader("Content-Length", "0");
--- RedirectHandler.hpp
+++ RedirectHandler.hpp
@@ -5,12 +5,12 @@
 
 class RedirectHandler : public IHandler {
  public:
-	explicit RedirectHandler(const Location& location);
-	virtual ~RedirectHandler();
+  explicit RedirectHandler(const Location& location);
+  virtual ~RedirectHandler();
 
-	virtual HandlerResult start(Connection& conn);
-	virtual HandlerResult resume(Connection& conn);
+  virtual HandlerResult start(Connection& conn);
+  virtual HandlerResult resume(Connection& conn);
 
  private:
-	Location location_;
+  Location location_;
 };
