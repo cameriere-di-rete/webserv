40a7501a53a0ea0c26dcbc46304f6aefbab70d15
S-0-L-0 <nonsocheemailscrivere9@gmail.com>
2025-11-25 15:26:41 +0100

    fix: correct response version assignment and resume return value in RedirectHandler

--- RedirectHandler.cpp
+++ RedirectHandler.cpp
@@ -13,6 +13,7 @@ RedirectHandler::~RedirectHandler() {}
 HandlerResult RedirectHandler::start(Connection& conn) {
   // Prepare redirect response and serialize into connection write buffer
   conn.response = Response();
+  conn.response.status_line.version = HTTP_VERSION;
   conn.response.status_line.status_code = location_.redirect_code;
   conn.response.status_line.reason =
       http::reasonPhrase(location_.redirect_code);
@@ -20,10 +21,11 @@ HandlerResult RedirectHandler::start(Connection& conn) {
   conn.response.getBody().data = "";
   conn.response.addHeader("Content-Length", "0");
   conn.write_buffer = conn.response.serialize();
+  conn.write_offset = 0;
   return HR_DONE;
 }
 
 HandlerResult RedirectHandler::resume(Connection& conn) {
   (void)conn;
-  return HR_ERROR;  // no resume functionality for redirects
+  return HR_DONE;  // no resume functionality for redirects
 }
